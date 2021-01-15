a7e79f1fcad711220a5e715dd39af1f5a26d94e7
Jiawei Wu <abc1929@gmail.com>
2021-01-16 00:53:54 +0800

    basics done

--- src/components/card.js
+++ src/components/card.js
@@ -1,33 +1,67 @@
+import axios from "axios";
+
 const Card = (article) => {
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
-}
+   // TASK 5
+   // ---------------------
+   // Implement this function, which should return the markup you see below.
+   // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
+   // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+   // The text inside elements will be set using their `textContent` property (NOT `innerText`).
+   // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
+   //
+   // <div class="card">
+   //   <div class="headline">{ headline }</div>
+   //   <div class="author">
+   //     <div class="img-container">
+   //       <img src={ authorPhoto }>
+   //     </div>
+   //     <span>By { authorName }</span>
+   //   </div>
+   // </div>
+   //
+
+   const div = document.createElement("div");
+   div.className = "card";
+   const div1 = document.createElement("div");
+   div1.className = "headline";
+   const div2 = document.createElement("div");
+   div2.textContent = article.headline;
+   div2.className = "author";
+   const div21 = document.createElement("div");
+   div21.className = "img-container";
+   const img = document.createElement("img");
+   img.src = article.authorPhoto;
+   const span = document.createElement("span");
+   span.textContent = article.authorName;
+
+   div21.append(img);
+   div2.append(div21, span);
+   div.append(div1, div2);
+   return div;
+};
 
 const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `https://lambda-times-api.herokuapp.com/articles`
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-}
+   // TASK 6
+   // ---------------------
+   // Implement this function that takes a css selector as its only argument.
+   // It should obtain articles from this endpoint: `https://lambda-times-api.herokuapp.com/articles`
+   // However, the articles do not come organized in a single, neat array. Inspect the response closely!
+   // Create a card from each and every article object in the response, using the Card component.
+   // Append each card to the element in the DOM that matches the selector passed to the function.
+   //
 
-export { Card, cardAppender }
+   axios
+      .get("https://lambda-times-api.herokuapp.com/articles")
+      .then((res) => {
+         //  console.log(res.data.articles);
+         // we figure out the keys of the object and descend one more layer until the article layer, which can be passed to Card() to create and render.
+         for (let i of Object.keys(res.data.articles)) {
+            res.data.articles[i].forEach((article) => {
+               document.querySelector(selector).append(Card(article));
+            });
+         }
+      })
+      .catch((err) => console.log(`Error: ${err}`));
+};
+
+export { Card, cardAppender };
